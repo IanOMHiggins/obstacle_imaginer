beb7b7aaa9742ba47e8516b891f1dd47eea5ed83
Ian Higgins <ianomhiggins@gmail.com>
2020-11-01 23:06:08 -0500

    removed a bunch of commented copied code

--- include/obstacle_imaginer/obstacle_imaginer.h
+++ include/obstacle_imaginer/obstacle_imaginer.h
@@ -60,61 +60,27 @@ private:
   // bool debug;
   
   // variables
-  // ChunkManager* chunk_manager;
-  // bool got_tracking_point, got_look_ahead;
-  // core_trajectory_msgs::Odometry tracking_point, look_ahead;
-  // templib::TimeOutMonitor* look_ahead_time_monitor;
-  // templib::TimeOutMonitor* all_in_collision_monitor;
-  // templib::TimeOutMonitor* stuck_monitor;
-  // templib::TemporalThresholdMonitor<tf::Vector3, double>* stationary_monitor;
-  // const int SMOOTH_YAW = 1;
-  // const int WAYPOINT_YAW = 2;
-  // int yaw_mode;
-  // WaypointManager* waypoint_manager;
   
   // robot code:
   // 11 = R1, 12 = R2, 13 = R3, 51 = DS1, 52 = DS2, 53 = DS3, 54 = DS4
   std::map<short, std::string> robot_codes;
+  const short ugv_lt_uav = 50;
 
 
   // services
   // ros::ServiceServer clear_map_server;
   
   // publishers
-  // TODO: Add way_point to communication_manager
+  // TODO: Add waypoint_point_vis to communication_manager
   ros::Publisher virtual_obstacle_pub;
 
   // subscribers
-  // ros::CallbackQueue cloud_callback_queue;
-  // ros::AsyncSpinner* spinner;
-  // ros::Subscriber cloud_sub;
-  // ros::Subscriber odom_sub;
-  // ros::Subscriber tracking_point_sub;
-  // ros::Subscriber look_ahead_sub;
-  // ros::Subscriber virtual_obstacle_sub;
-  // ros::Subscriber reset_stuck_sub;
-  // ros::Timer health_timer;
-  // tf::TransformListener* listener;
-  // std::vector<message_filters::Subscriber<sensor_msgs::Image>* > depth_image_subs;
-  // std::vector<message_filters::Subscriber<sensor_msgs::CameraInfo>* > camera_info_subs;
-  // std::vector<message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::CameraInfo>* > depth_image_syncs;
   std::vector<ros::Subscriber> odom_subs;
   std::vector<ros::Subscriber> way_point_subs;
   
   // callbacks
   void odom_callback(const nav_msgs::Odometry& msg, short robot_code);
   void way_point_callback(const geometry_msgs::PointStamped& msg, short robot_code);
-  // void cloud_callback(const sensor_msgs::PointCloud2& msg);
-  // void odom_callback(nav_msgs::Odometry msg);
-  // void tracking_point_callback(core_trajectory_msgs::Odometry msg);
-  // void look_ahead_callback(core_trajectory_msgs::Odometry msg);
-  // void timer_callback(const ros::TimerEvent& event);
-  // void depth_image_callback(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::CameraInfoConstPtr& cam_info);
-  // void virtual_obstacle_callback(const sensor_msgs::PointCloud2& msg);
-  // void reset_stuck_callback(const std_msgs::Empty& msg);
-  // bool clear_map_callback(std_srvs::SetBool::Request& request, std_srvs::SetBool::Response& response);
-  
-  // void set_yaw(core_trajectory_msgs::TrajectoryXYZVYaw& trajectory, const geometry_msgs::PoseStamped& waypoint);
   
 public:
   ObstacleImaginer(std::string node_name);
--- src/obstacle_imaginer.cpp
+++ src/obstacle_imaginer.cpp
@@ -7,7 +7,6 @@ ObstacleImaginer::ObstacleImaginer(std::string node_name)
 
 bool ObstacleImaginer::initialize(){
 
-
   robot_codes[(short)11] = "ugv1";
   robot_codes[(short)12] = "ugv2";
   robot_codes[(short)13] = "ugv3";
@@ -15,7 +14,6 @@ bool ObstacleImaginer::initialize(){
   robot_codes[(short)52] = "uav2";
   robot_codes[(short)53] = "uav3";
   robot_codes[(short)54] = "uav4";
-
 
   ros::NodeHandle* nh = get_node_handle();
   ros::NodeHandle* pnh = get_private_node_handle();
@@ -27,37 +25,17 @@ bool ObstacleImaginer::initialize(){
   // init publishers
   virtual_obstacle_pub = nh->advertise<sensor_msgs::PointCloud2>("virtual_obstacles", 1);
 
-  // debug_markers_pub = nh->advertise<visualization_msgs::MarkerArray>("debug_markers2", 1);
-  // health_pub = nh->advertise<std_msgs::Bool>("opencl_pointcloud_map_health2", 1);
-  // traj_pub = nh->advertise<core_trajectory_msgs::TrajectoryXYZVYaw>("trajectory", 1);
-  // stuck_pub = nh->advertise<std_msgs::Bool>("stuck", 1);
-  // waypoint_pose_pub = nh->advertise<geometry_msgs::PoseStamped>("waypoint_pose_vis", 1);
-  // waypoint_point_pub = nh->advertise<geometry_msgs::PointStamped>("waypoint_point_vis", 1);
-  // map_clearing_point_pub = nh->advertise<geometry_msgs::PoseStamped>("map_clearing_point", 1);
   
   // init subscribers
-  // cloud_sub = nh->subscribe("pointcloud", 1, &ObstacleImaginer::cloud_callback, this);
-  // odom_sub = nh->subscribe("odometry", 1, &ObstacleImaginer::odom_callback, this);
-  // tracking_point_sub = nh->subscribe("tracking_point", 1, &ObstacleImaginer::tracking_point_callback, this);
-  // look_ahead_sub = nh->subscribe("look_ahead", 1, &ObstacleImaginer::look_ahead_callback, this);
-  // virtual_obstacle_sub = nh->subscribe("virtual_obstacles", 1, &ObstacleImaginer::virtual_obstacle_callback, this);
-  // reset_stuck_sub = nh->subscribe("reset_stuck", 1, &ObstacleImaginer::reset_stuck_callback, this);
-  // health_timer = nh->createTimer(ros::Duration(0.1), &ObstacleImaginer::timer_callback, this);
-  // listener = new tf::TransformListener();
-  // std::vector<ros::Subscriber> odom_subs;
-  // std::vector<ros::Subscriber> way_point_subs;
   for (std::pair<short, std::string>& r : robot_codes){
     odom_subs.push_back(nh.subscribe(r.second + "/integrated_to_map", 1, 
       boost::bind(&ObstacleImaginer::odom_callback, _1, r.first), this));
-    way_point_subs.push_back(nh.subscribe(r.second + "/way_point", 1, 
+    way_point_subs.push_back(nh.subscribe(r.second + "/waypoint_point_vis", 1, 
       boost::bind(&ObstacleImaginer::way_point_callback, _1, r.first), this));
   }
-  
 
   listener = new tf::TransformListener();
   
-  
-
   return true;
 }
 
@@ -71,11 +49,14 @@ bool ObstacleImaginer::execute(){
 void odom_callback(const nav_msgs::Odometry& msg, short robot_code){
   // robot code:
   // 11 = R1, 12 = R2, 13 = R3, 51 = DS1, 52 = DS2, 53 = DS3, 54 = DS4
+
+  // if code < 50, we know it's a ugv.
+  if (robot_code < ugv_lt_uav){
+    virtual_obstacle_pub.publish()
+  }
   
 
-
 }
-
 
   // callbacks
 void way_point_callback(const geometry_msgs::PointStamped& msg, short robot_code){
